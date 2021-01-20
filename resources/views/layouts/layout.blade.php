d35dc76580f059452938439e4b1420626c9a62c3
Alan <>
2021-01-20 05:19:19 +0000

    Added layout and success blade that is returned by a get route, with the users first name. There is now CSS styling with a single breakpoint that allows the content to be responsive down to 320px.

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Signup</title>
+
+    <link href="{{ URL::asset('/css/style.css') }}" rel="stylesheet" type="text/css">
+</head>
+
+<body class="antialiased">
+    <div class="container">
+        <div class="panel">
+
+            @yield('signup')
+
+        </div>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('store', function(Request $request) {
+    $firstName = explode(' ', $request['full-name'])[0];
+    return view('success', ['name' => $firstName]);
+});
+
+// Route::post('store', [Controller::class, 'store']);
